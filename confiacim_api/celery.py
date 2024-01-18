409fabcab2e300962e3200fde57dbb17139d8f3c
Henrique C. C. de Andrade <henrique.ccda@gmail.com>
2024-01-18 00:38:09 -0300

    [FEAT: #15] Armazenando o id da task do celery.
    closes #15

--- confiacim_api/celery.py
+++ confiacim_api/celery.py
@@ -1,9 +1,6 @@
 from celery import Celery
-from sqlalchemy import select
 
 from confiacim_api.conf import settings
-from confiacim_api.database import SessionFactory
-from confiacim_api.models import Simulation
 
 celery_app = Celery(
     __name__,
@@ -12,19 +9,4 @@ celery_app = Celery(
     broker_connection_retry_on_startup=True,  # TODO: Pesquisar essa configuração.
 )
 
-
-# TODO: testa unitariamente
-@celery_app.task()
-def simulation_run(simulation_id) -> dict[str, str]:
-    tag = None
-
-    with SessionFactory() as session:
-        db_simulation = session.scalar(select(Simulation).where(Simulation.id == simulation_id))
-        if db_simulation:
-            tag = db_simulation.tag
-
-    import time
-
-    time.sleep(20)
-
-    return {"status": f"Simulação {tag} rodou."}
+celery_app.autodiscover_tasks(packages=["confiacim_api"])
