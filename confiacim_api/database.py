f2d5ab5ff61433fcd464551b3803fc407a6f66c2
Henrique C. C. de Andrade <henrique.ccda@gmail.com>
2024-01-17 22:00:14 -0300

    [CONFIG: #5] - Configurando o celery.
    close #5

--- confiacim_api/database.py
+++ confiacim_api/database.py
@@ -2,15 +2,17 @@ from typing import Annotated
 
 from fastapi import Depends
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, sessionmaker
 
 from confiacim_api.conf import settings
 
 engine = create_engine(settings.DATABASE_URL, echo=settings.SQLALCHEMY_ECHO)
 
+SessionFactory = sessionmaker(engine)
+
 
 def get_session():
-    with Session(engine) as session:
+    with SessionFactory() as session:
         yield session
 
 
