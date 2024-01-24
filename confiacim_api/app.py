8e11a674bf8bb9921b3f952fda132bab32d6917a
Henrique C. C. de Andrade <henrique.ccda@gmail.com>
2024-01-24 17:30:49 -0300

    [CONFIG: #29] - Integrando o docker compose do frontend com o do backend.
    close #29

--- confiacim_api/app.py
+++ confiacim_api/app.py
@@ -1,17 +1,17 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 
 from confiacim_api.routes import base_router, celery_router, simulation_router
 
-app = FastAPI()
+app = FastAPI(docs_url="/api/docs", openapi_url="/api/openapi.json")
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins="http://localhost:5173",
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# from fastapi.middleware.cors import CORSMiddleware
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins="http://localhost:5173",
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 app.include_router(base_router)
 app.include_router(simulation_router)
