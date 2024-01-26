28e5bc89b6903078394c7a999dabcad1d92a6391
Henrique C. C. de Andrade <henrique.ccda@gmail.com>
2024-01-25 23:15:56 -0300

    [REFAC: #28] - Criar a variável de ambiente para o CORS.
    close #28

--- confiacim_api/app.py
+++ confiacim_api/app.py
@@ -1,17 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
+from confiacim_api.conf import settings
 from confiacim_api.routes import base_router, celery_router, simulation_router
 
 app = FastAPI(docs_url="/api/docs", openapi_url="/api/openapi.json")
 
-# from fastapi.middleware.cors import CORSMiddleware
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins="http://localhost:5173",
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
+if settings.CORS:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=settings.CORS,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
 app.include_router(base_router)
 app.include_router(simulation_router)
