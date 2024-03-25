e255dc117ac4b04f0ef9ff24efa3a4fef8e080b1
Henrique C. C. de Andrade <henrique.ccda@gmail.com>
2024-03-24 22:48:55 -0300

    [FEAT: #37] - Cria rota checagem da api.
    close #37

--- confiacim_api/routes/base.py
+++ confiacim_api/routes/base.py
@@ -1,8 +1,26 @@
 from fastapi import APIRouter
+from sqlalchemy import text
+from sqlalchemy.exc import OperationalError
+
+from confiacim_api.database import ActiveSession
+from confiacim_api.schemas import Health, Message
 
 router = APIRouter(prefix="/api", tags=["Base"])
 
 
-@router.get("/")
+def check_db(session: ActiveSession) -> bool:
+    try:
+        session.execute(text("select 1+1"))
+    except OperationalError:
+        return False
+    return True
+
+
+@router.get("/", response_model=Message)
 def index():
     return {"message": "Api do confiacim"}
+
+
+@router.get("/heath", response_model=Health)
+def health(session: ActiveSession):
+    return {"status": "ok" if check_db(session) else "fail"}
--- tests/routes/test_base.py
+++ tests/routes/test_base.py
@@ -2,12 +2,28 @@ from confiacim_api.app import app
 from fastapi import status
 from fastapi.testclient import TestClient
 
-URL = app.url_path_for("index")
 
-
-def test_index(client: TestClient):
-    resp = client.get(URL)
+def test_positive_index(client: TestClient):
+    resp = client.get(app.url_path_for("index"))
 
     assert resp.status_code == status.HTTP_200_OK
 
     assert resp.json() == {"message": "Api do confiacim"}
+
+
+def test_positive_health(client: TestClient):
+    resp = client.get(app.url_path_for("health"))
+
+    assert resp.status_code == status.HTTP_200_OK
+
+    assert resp.json() == {"status": "ok"}
+
+
+def test_negative_health(mocker, client: TestClient):
+    mocker.patch("confiacim_api.routes.base.check_db", return_value=False)
+
+    resp = client.get(app.url_path_for("health"))
+
+    assert resp.status_code == status.HTTP_200_OK
+
+    assert resp.json() == {"status": "fail"}
