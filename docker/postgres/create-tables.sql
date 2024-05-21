8d7f5ce3e86a1811aec57a562c51cc93314b89bb
Henrique C. C. de Andrade <henrique.ccda@gmail.com>
2024-05-20 21:52:43 -0300

    [CONFIG: #60] - Add o alembic.
    close #60

--- docker/postgres/create-tables.sql
+++ docker/postgres/create-tables.sql
@@ -1,9 +1,20 @@
-\c confiacim_api
+BEGIN;
 
-CREATE TABLE IF NOT EXISTS simulation (
+CREATE TABLE alembic_version (
+    version_num VARCHAR(32) NOT NULL,
+    CONSTRAINT alembic_version_pkc PRIMARY KEY (version_num)
+);
+
+-- Running upgrade  -> 01124ddc640a
+
+CREATE TABLE simulation (
     id SERIAL NOT NULL,
     tag VARCHAR(30) NOT NULL,
     celery_task_id UUID,
     PRIMARY KEY (id),
     UNIQUE (tag)
 );
+
+INSERT INTO alembic_version (version_num) VALUES ('01124ddc640a') RETURNING alembic_version.version_num;
+
+COMMIT;
