fdaa3fa9d1967abbe0886b92ac2e839979e3a9a6
Henrique C. C. de Andrade <henrique.ccda@gmail.com>
2024-07-15 21:58:07 -0300

    [FEAT: #100] - Extrai os arquivos de simulação do blob do banco de dados
    close #100

--- confiacim_api/files_and_folders_handlers.py
+++ confiacim_api/files_and_folders_handlers.py
@@ -1,7 +1,10 @@
+from io import BytesIO
 from pathlib import Path
 from tempfile import TemporaryDirectory
 from typing import BinaryIO
 from zipfile import ZipFile
+
+from confiacim_api.models import Case
 
 
 def temporary_simulation_folder(origin_dir: Path) -> TemporaryDirectory:
@@ -34,3 +37,15 @@ def unzip_file(file: BinaryIO, temp_folder: TemporaryDirectory):
 def clean_temporary_simulation_folder(dir: TemporaryDirectory):
     """Limpa o diretorio temporario"""
     dir.cleanup()
+
+
+def unzip_tencim_case(case: Case, tmp_dir: TemporaryDirectory):
+    """
+    Unzip o caso da simulação do Tencim do blob salvo no banco de Dados
+
+    Parameters:
+        case: Caso (ORM)
+        tmp_dir: Diretorio temporario
+    """
+    file_like = BytesIO(case.base_file)
+    unzip_file(file_like, tmp_dir)
