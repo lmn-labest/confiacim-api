b2244ac32af6059ee96a6713a1ac42ec84ee9af6
Henrique C. C. de Andrade <henrique.ccda@gmail.com>
2024-07-15 21:58:07 -0300

    [FEAT: #100] - Extrai os arquivos de simulação do blob do banco de dados
    close #100

--- tests/test_files_and_folders_handlers.py
+++ tests/test_files_and_folders_handlers.py
@@ -2,12 +2,15 @@ from pathlib import Path
 from tempfile import TemporaryDirectory
 
 import pytest
+from sqlalchemy import select
 
 from confiacim_api.files_and_folders_handlers import (
     clean_temporary_simulation_folder,
     temporary_simulation_folder,
     unzip_file,
+    unzip_tencim_case,
 )
+from confiacim_api.models import Case
 
 
 @pytest.mark.integration
@@ -59,3 +62,35 @@ def test_positive_create__unzip_and_clean_simulation_folder(tmp_path):
     list_dir = list(tmp_path.iterdir())
 
     assert len(list_dir) == 0
+
+
+@pytest.mark.integration
+def test_unzip_tencim_case_from_db(session, user, tmp_path):
+
+    tmp_dir = temporary_simulation_folder(tmp_path)
+
+    with open("tests/fixtures/case1.zip", mode="rb") as fp:
+        case = Case(tag="case1", user=user, base_file=fp.read())
+        session.add(case)
+        session.commit()
+        session.reset()
+
+    case_from_db = session.scalar(select(Case).where(Case.tag == "case1"))
+
+    unzip_tencim_case(case_from_db, tmp_dir)
+
+    list_dir = {d.name for d in Path(tmp_dir.name).iterdir()}
+
+    assert len(list_dir) == 7
+
+    expected = {
+        "loads.dat",
+        "initialtemperature.dat",
+        "case.dat",
+        "adiabat.dat",
+        "mesh.dat",
+        "initialstress.dat",
+        "materials.dat",
+    }
+
+    assert list_dir == expected
