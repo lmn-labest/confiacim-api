a1ca13e82191d1f83626dac74fca5631b52a7f1d
Henrique C. C. de Andrade <henrique.ccda@gmail.com>
2024-09-14 19:01:21 -0300

    [TEST: #146] - Testa a função currentUser.
    close #146

--- tests/test_security.py
+++ tests/test_security.py
@@ -1,9 +1,16 @@
 import pytest
+from fastapi import HTTPException
 from freezegun import freeze_time
 from jwt import ExpiredSignatureError, decode
 
 from confiacim_api.conf import settings
-from confiacim_api.security import create_access_token, get_password_hash, verify_password
+from confiacim_api.models import User
+from confiacim_api.security import (
+    create_access_token,
+    get_current_user,
+    get_password_hash,
+    verify_password,
+)
 
 PASSWORD = "123456"
 
@@ -51,3 +58,48 @@ def test_expire_time_jwt():
     with freeze_time("2012-01-14 02:00:00"):
         with pytest.raises(ExpiredSignatureError):
             decode(token, settings.JWT_SECRET_KEY, algorithms=["HS256"])
+
+
+@pytest.mark.asyncio
+async def test_positive_get_current_user(session, user: User, token: str):
+
+    user = await get_current_user(session, token)
+
+    assert user.email == user.email
+    assert not user.is_admin
+
+
+@pytest.mark.asyncio
+async def test_negative_get_current_user_invalid_token(session, user: User):
+
+    with pytest.raises(HTTPException, match="401: Not authenticated"):
+        await get_current_user(session, "invalid_token")
+
+
+@pytest.mark.asyncio
+async def test_negative_get_current_user_token_without_sub(session, user: User):
+
+    token = create_access_token(
+        data={
+            "email": user.email,
+            "admin": user.is_admin,
+        }
+    )
+
+    with pytest.raises(HTTPException, match="401: Not authenticated"):
+        await get_current_user(session, token)
+
+
+@pytest.mark.asyncio
+async def test_negative_get_current_user_token_sub_invalid_id(session, user: User):
+
+    token = create_access_token(
+        data={
+            "sub": user.id + 1,
+            "email": user.email,
+            "admin": user.is_admin,
+        }
+    )
+
+    with pytest.raises(HTTPException, match="401: Not authenticated"):
+        await get_current_user(session, token)
